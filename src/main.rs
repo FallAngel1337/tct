917e947f429fb2653c98207cd66cf13b33573244
FallAngel1337 <fallangel@protonmail.com>
2021-11-28 08:34:54 -0300

    Added a setup option to install the manpages

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,19 @@
 use std::{env, process};
 use std::error::Error;
 use tct::cmd::COMMANDS;
+use tct::cmd::man;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
+
+    if &args[1] == "setup" {
+        if  args.len() >= 3 {
+            man::install_manpages(&args[2], "/opt/man")?;
+            process::exit(0);
+        }
+        process::exit(1);
+    }
+
     COMMANDS
         .get(args.get(1).unwrap_or_else(|| {
             eprintln!("need to pass an argument");
