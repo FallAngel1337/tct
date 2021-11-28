a7adc8d9b99ab5050c85e205c6576cfa058e249d
FallAngel1337 <fallangel@protonmail.com>
2021-11-28 08:24:52 -0300

    Adding install_manpages function and fixed the if conditional

--- src/cmd/man.rs
+++ src/cmd/man.rs
@@ -1,14 +1,33 @@
 use crate::cmd::ProgResult;
 use crate::man::MANUALS;
-use std::io::{stdout, Write};
+use std::{
+        fs, 
+        io::{self, stdout, Write},
+        path::Path
+};
+
+pub fn install_manpages<T: AsRef<Path>, U: AsRef<Path>>(from: T, to: U) -> io::Result<()> {
+    fs::create_dir_all(&to)?;
+    
+    for entry in fs::read_dir(from)? {
+            let entry = entry.unwrap();
+            if entry.file_type().unwrap().is_dir() {
+                install_manpages(entry.path(), to.as_ref().join(entry.file_name()))?;
+            } else {
+                fs::copy(entry.path(), to.as_ref().join(entry.file_name()))?;
+            }
+    };
+
+    Ok(())
+}
 
 pub fn man(cmds: &[String]) -> ProgResult {
     let out = stdout();
     let mut out = out.lock();
 
     cmds.iter().skip(1).for_each(|cmd| {
-        if !MANUALS.contains_key(cmd.as_str()) {
-            out.write_all(MANUALS[cmd.as_str()].as_bytes())
+        if MANUALS.contains_key(cmd.as_str()) {
+            out.write_all(fs::read_to_string(MANUALS[cmd.as_str()]).unwrap().as_bytes())
                 .expect("Cannot show manual");
         }
     });
